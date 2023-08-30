ab3e7ec9d843b98c195983c80a9e799b89d3c7c6
DevExpressExampleBot <fakeEmail>
2023-08-30 13:00:26 +0400

    Source auto update [skip ci]

--- VB/Busy/BusyForm.Designer.vb
+++ VB/Busy/BusyForm.Designer.vb
@@ -1,73 +1,69 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
 Namespace Busy
-	Partial Public Class BusyForm
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class BusyForm
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.realProgress1 = New Busy.ProgressControl()
-			Me.simpleButton1 = New DevExpress.XtraEditors.SimpleButton()
-			Me.SuspendLayout()
-			' 
-			' realProgress1
-			' 
-			Me.realProgress1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.realProgress1.IsOperationInProgress = False
-			Me.realProgress1.Location = New System.Drawing.Point(0, 0)
-			Me.realProgress1.Name = "realProgress1"
-			Me.realProgress1.Size = New System.Drawing.Size(556, 97)
-			Me.realProgress1.TabIndex = 3
-'			Me.realProgress1.Load += New System.EventHandler(Me.realProgress1_Load);
-			' 
-			' simpleButton1
-			' 
-			Me.simpleButton1.Dock = System.Windows.Forms.DockStyle.Bottom
-			Me.simpleButton1.Location = New System.Drawing.Point(0, 97)
-			Me.simpleButton1.Name = "simpleButton1"
-			Me.simpleButton1.Size = New System.Drawing.Size(556, 23)
-			Me.simpleButton1.TabIndex = 4
-			Me.simpleButton1.Text = "Can click"
-'			Me.simpleButton1.Click += New System.EventHandler(Me.simpleButton1_Click);
-			' 
-			' BusyForm
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(556, 120)
-			Me.Controls.Add(Me.realProgress1)
-			Me.Controls.Add(Me.simpleButton1)
-			Me.Name = "BusyForm"
-			Me.Text = "Busy Form"
-'			Me.FormClosing += New System.Windows.Forms.FormClosingEventHandler(Me.BusyForm_FormClosing);
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+'#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.realProgress1 = New Busy.ProgressControl()
+            Me.simpleButton1 = New DevExpress.XtraEditors.SimpleButton()
+            Me.SuspendLayout()
+            ' 
+            ' realProgress1
+            ' 
+            Me.realProgress1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.realProgress1.IsOperationInProgress = False
+            Me.realProgress1.Location = New System.Drawing.Point(0, 0)
+            Me.realProgress1.Name = "realProgress1"
+            Me.realProgress1.Size = New System.Drawing.Size(556, 97)
+            Me.realProgress1.TabIndex = 3
+            ' 
+            ' simpleButton1
+            ' 
+            Me.simpleButton1.Dock = System.Windows.Forms.DockStyle.Bottom
+            Me.simpleButton1.Location = New System.Drawing.Point(0, 97)
+            Me.simpleButton1.Name = "simpleButton1"
+            Me.simpleButton1.Size = New System.Drawing.Size(556, 23)
+            Me.simpleButton1.TabIndex = 4
+            Me.simpleButton1.Text = "Can click"
+            AddHandler Me.simpleButton1.Click, New System.EventHandler(AddressOf Me.simpleButton1_Click)
+            ' 
+            ' BusyForm
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(556, 120)
+            Me.Controls.Add(Me.realProgress1)
+            Me.Controls.Add(Me.simpleButton1)
+            Me.Name = "BusyForm"
+            Me.Text = "Busy Form"
+            AddHandler Me.FormClosing, New System.Windows.Forms.FormClosingEventHandler(AddressOf Me.BusyForm_FormClosing)
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private WithEvents realProgress1 As ProgressControl
-		Private WithEvents simpleButton1 As DevExpress.XtraEditors.SimpleButton
+'#End Region
+        Private realProgress1 As Busy.ProgressControl
 
-	End Class
+        Private simpleButton1 As DevExpress.XtraEditors.SimpleButton
+    End Class
 End Namespace
-
--- VB/Busy/ProgressControl.vb
+++ VB/Busy/ProgressControl.vb
@@ -1,67 +1,63 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Drawing
-Imports System.Data
-Imports System.Linq
-Imports System.Text
 Imports System.Windows.Forms
+Imports System.Threading
 
 Namespace Busy
-	Partial Public Class ProgressControl
-		Inherits UserControl
-		Private _IsOperationInProgress As Boolean
-		Public Property IsOperationInProgress() As Boolean
-			Get
-				Return _IsOperationInProgress
-			End Get
-			Set(ByVal value As Boolean)
-				_IsOperationInProgress = value
-				simpleButton1.Enabled = Not value
-			End Set
-		End Property
 
-		Private Const INT_filesCount As Integer = 500
-		Private Const INT_operationTime As Integer = 10
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Public Partial Class ProgressControl
+        Inherits UserControl
 
-		Private Shared Sub CopyFile()
-			System.Threading.Thread.Sleep(INT_operationTime)
-		End Sub
+        Private _IsOperationInProgress As Boolean
 
-		Private Sub UpdateProgressBar(ByVal value As Integer)
-			BeginInvoke(New MethodInvoker(Function() AnonymousMethod1(value)))
-		End Sub
-		
-		Private Function AnonymousMethod1(ByVal value As Integer) As Boolean
-			progressBarControl1.EditValue = value
-			Return True
-		End Function
+        Public Property IsOperationInProgress As Boolean
+            Get
+                Return _IsOperationInProgress
+            End Get
 
-		Private Sub backgroundWorker_DoWork(ByVal sender As Object, ByVal e As DoWorkEventArgs) Handles backgroundWorker.DoWork
-			For i As Integer = 1 To INT_filesCount
-				CopyFile()
-				Dim progress As Integer = i * 100 \ INT_filesCount
-				backgroundWorker.ReportProgress(progress)
-			Next i
-		End Sub
+            Set(ByVal value As Boolean)
+                _IsOperationInProgress = value
+                simpleButton1.Enabled = Not value
+            End Set
+        End Property
 
+        Private Const INT_filesCount As Integer = 500
 
-		Private Sub backgroundWorker_ProgressChanged(ByVal sender As Object, ByVal e As ProgressChangedEventArgs) Handles backgroundWorker.ProgressChanged
-			UpdateProgressBar(e.ProgressPercentage)
-		End Sub
+        Private Const INT_operationTime As Integer = 10
 
-		Private Sub backgroundWorker_Completed(ByVal sender As Object, ByVal e As RunWorkerCompletedEventArgs) Handles backgroundWorker.RunWorkerCompleted
-			UpdateProgressBar(0)
-			IsOperationInProgress = False
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		Private Sub simpleButton1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles simpleButton1.Click
-			IsOperationInProgress = True
-			backgroundWorker.RunWorkerAsync()
-		End Sub
-	End Class
+        Private Shared Sub CopyFile()
+            Thread.Sleep(INT_operationTime)
+        End Sub
+
+        Private Sub UpdateProgressBar(ByVal value As Integer)
+            progressBarControl1.EditValue = value
+        End Sub
+
+        Private Sub backgroundWorker_DoWork(ByVal sender As Object, ByVal e As DoWorkEventArgs)
+            For i As Integer = 1 To INT_filesCount
+                Call CopyFile()
+                Dim progress As Integer = i * 100 \ INT_filesCount
+                backgroundWorker.ReportProgress(progress)
+            Next
+        End Sub
+
+        Private Sub backgroundWorker_ProgressChanged(ByVal sender As Object, ByVal e As ProgressChangedEventArgs)
+            UpdateProgressBar(e.ProgressPercentage)
+        End Sub
+
+        Private Sub backgroundWorker_Completed(ByVal sender As Object, ByVal e As RunWorkerCompletedEventArgs)
+            UpdateProgressBar(0)
+            IsOperationInProgress = False
+        End Sub
+
+        Private Sub simpleButton1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            IsOperationInProgress = True
+            backgroundWorker.RunWorkerAsync()
+        End Sub
+    End Class
 End Namespace
